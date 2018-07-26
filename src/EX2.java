bff78b181d8116069c1133793de01b1cdb0ca58e
Myiah Moore <myiahsade@gmail.com>
2018-07-25 21:21:00 -0500

    check in

---
+++ src/EX2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EX2 {
+    public static void main(String[] args) {
+        String[] names = {"Bob", "Kenn", "Kevin", "Erin"};
+        ArrayList<String> studentList = new ArrayList<String>();
+        studentList.addAll(Arrays.asList(names));
+        printStudentNames=(studentList);
+
+    }
+
+    public static void String (printStudentName(ArrayList<String> anArray) {
+        for ( String name: names) {
+            System.out.println(name);
+        }
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.text.Style;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int lastNumber = 100;
+        System.out.println("Starting Count to " + lastNumber);
+        countTheNumbers(lastNumber);
+    }
+   public static void countTheNumbers(int last)
+    {
+        for (int first=0; first<last;first++);
+        {
+        }
+    }
+
+
+}
